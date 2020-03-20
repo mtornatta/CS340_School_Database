f2aa4eabedcb7250df5f75da548561627563d86a
Michael Tornatta <michaeltornatta@gmail.com>
2020-03-19 20:36:27 -0700

    Completed all data display

--- app.py
+++ app.py
@@ -8,53 +8,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cs340_tornattm:2266@cla
 engine = create_engine('mysql+pymysql://cs340_tornattm:2266@classmysql.engr.oregonstate.edu:3306/cs340_tornattm')
 db = SQLAlchemy(app)
 
-# class school(db.Model):
-# 	first_name = db.Column(db.String(255))
-# 	last_name = db.Column(db.String(255))
-# 	student_id = db.Column(db.Integer, primary_key=True)
-# 	year = db.Column(db.Integer)
-# 	major = db.Column(db.String(255))
-# 	email = db.Column(db.String(255))
-# 	school_name = db.Column(db.String(255))
-
-# class teacher(db.Model):
-# 	first_name = db.Column(db.String(255))
-# 	last_name = db.Column(db.String(255))
-# 	student_id = db.Column(db.Integer, primary_key=True)
-# 	year = db.Column(db.Integer)
-# 	major = db.Column(db.String(255))
-# 	email = db.Column(db.String(255))
-# 	school_name = db.Column(db.String(255))
-
-class student(db.Model):
-	first_name = db.Column(db.String(255))
-	last_name = db.Column(db.String(255))
-	student_id = db.Column(db.Integer, primary_key=True)
-	year = db.Column(db.Integer)
-	major = db.Column(db.String(255))
-	email = db.Column(db.String(255))
-	school_name = db.Column(db.String(255))
-
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/classes')
 def classes():
-	return render_template('classes.html')
+	class_data = db.engine.execute("SELECT class.course_id, class.course_name, class_teacher.tid, class.number_of_students, class.available_seats FROM class LEFT JOIN class_teacher ON class.course_id = class_teacher.cid;")
+	return render_template('classes.html', class_list=class_data)
 
 @app.route('/teachers')
 def teachers():
-	return render_template('teachers.html')
+	teacher_data = db.engine.execute("SELECT teacher.teacher_id, concat(first_name, ' ', last_name) AS Name, class_teacher.cid FROM teacher LEFT JOIN class_teacher ON teacher.teacher_id = class_teacher.tid;")
+	return render_template('teachers.html',teacher_list=teacher_data)
 
 @app.route('/students', methods=['POST','GET'])
 def students():
-	all_students = db.engine.execute("SELECT student.student_id, concat(first_name,' ', last_name) AS Name, class_student.cid FROM student LEFT JOIN class_student ON student.student_id = class_student.sid;")
-	return render_template('students.html',student_list=all_students)
+	student_data = db.engine.execute("SELECT student.student_id, concat(first_name,' ', last_name) AS Name, class_student.cid FROM student LEFT JOIN class_student ON student.student_id = class_student.sid;")
+	return render_template('students.html',student_list=student_data)
 
 @app.route('/schools')
 def schools():
-	return render_template('schools.html')
+	school_data = db.engine.execute("SELECT school_name, division FROM school;")
+	return render_template('schools.html', school_list=school_data)
 
 if __name__ == "__main__":
 	app.run(debug=True)
