ae05926ed3857cd256e29e76635373314a0c555c
Michael Tornatta <michaeltornatta@gmail.com>
2020-03-19 22:20:42 -0700

    Got rid of some uneccesary comments

--- app.py
+++ app.py
@@ -8,24 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cs340_tornattm:2266@cla
 engine = create_engine('mysql+pymysql://cs340_tornattm:2266@classmysql.engr.oregonstate.edu:3306/cs340_tornattm')
 db = SQLAlchemy(app)
 
-# class school(db.Model):
-# 	first_name = db.Column(db.String(255))
-# 	last_name = db.Column(db.String(255))
-# 	student_id = db.Column(db.Integer, primary_key=True)
-# 	year = db.Column(db.Integer)
-# 	major = db.Column(db.String(255))
-# 	email = db.Column(db.String(255))
-# 	school_name = db.Column(db.String(255))
-
-# class teacher(db.Model):
-# 	first_name = db.Column(db.String(255))
-# 	last_name = db.Column(db.String(255))
-# 	student_id = db.Column(db.Integer, primary_key=True)
-# 	year = db.Column(db.Integer)
-# 	major = db.Column(db.String(255))
-# 	email = db.Column(db.String(255))
-# 	school_name = db.Column(db.String(255))
-
 class student(db.Model):
 	first_name = db.Column(db.String(255))
 	last_name = db.Column(db.String(255))
